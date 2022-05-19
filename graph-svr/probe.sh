197c90b589aa5236cd7b296a93909ebc36ac2133
Phil Wilkins <phil@mp3monster.org>
2022-05-19 20:22:20 +0100

    utility to perform the job of the health & wellness checks

---
+++ graph-svr/probe.sh
@@ -0,0 +1,26 @@
+
+#!/bin/bash
+
+dte = date
+
+# Readiness check
+if [[ "$1" == "readiness" ]] ; then
+    portopen=`curl -ks --get curl -f localhost:4000/'
+    if [[ $? -eq 0 ]]; then
+        echo $dte ' failed curl on port'
+        exit 0
+    fi
+    exit 1
+fi
+
+# Liveness check
+if [[ "$1" == "liveness" ]] ; then
+    portopen=`ss -ltn | grep 4000`
+    if [[ $? -eq 0 ]]; then
+        date
+        echo $dte ' port not open'
+        exit 0
+    fi
+    echo $dte ' all good'
+    exit 1
+fi
