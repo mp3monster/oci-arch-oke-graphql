2a4791e0be47ece42772619c67a91792fde03b75
Phil Wilkins <phil@mp3monster.org>
2022-05-25 21:23:27 +0100

    providing build & deploy automation

--- event-data-svc/deploy-paramd.sh
+++ event-data-svc/deploy-paramd.sh
@@ -1,10 +1,17 @@
+#!/bin/sh
 # this is differs from the dos approach as running on a mac we need to use the docker buildx feature to build for amd64 rather than arm64
 #docker build -t event-data-svc:latest .  # this is for a local build
-# parameter $1 is the username $2 is the token/password
+# parameter $1 is the username 
+# $2 is the token/password
+# $3 is the Tenancy name
+# $4 is the OCI Region in short form e.g. iad
+
+name=graphql-svr-svc
+
 echo "deploying for  $1"
-docker login -u ociobenablement/identitycloudservice/p$1 -p $2 iad.ocir.io
-docker buildx build --platform linux/amd64 --push -t iad.ocir.io/ociobenablement/event-data-svc:latest .
-# docker buildx build --platform linux/amd64 --push -t iad.ocir.io/ociobenablement/event-data-svc:latest
-docker logout iad.ocir.io/ociobenablement/
+docker login -u ${3}/identitycloudservice/${1} -p $2 ${4}.ocir.io
+docker buildx build --platform linux/amd64 --push -t ${4}.ocir.io/${3}/event-data-svc:latest .
+# docker buildx build --platform linux/amd64 --push -t ${4}.ocir.io/${3}/${name}:latest
+docker logout $4.ocir.io/${3}/
 kubectl apply -f ./deployment.yaml
-kubectl apply -f ./event-data-svc.yaml
+kubectl apply -f ./${name}.yaml
--- graph-svr/deploy-paramd.sh
+++ graph-svr/deploy-paramd.sh
@@ -1,11 +1,19 @@
+#!/bin/sh
 # this is differs from the dos approach as running on a mac we need to use the docker buildx feature to build for amd64 rather than arm64
 # docker build -t event-data-svc:latest .  # this is for a local build
-# parameter $1 is the username $2 is the token/password
+# Paramters are:
+# $1 is the username 
+# $2 is the token/password
+# $3 is the Tenancy name  we.g. myTenancy
+# $4 is the OCI Region in short form e.g. iad
+
+name=graphql-svr-svc
+
 echo "deploying for  $1"
-docker login -u ociobenablement/identitycloudservice/$1 -p $2  iad.ocir.io
-docker buildx build --platform linux/amd64 --push -t iad.ocir.io/ociobenablement/graphql-svr:latest .
-#docker logout iad.ocir.io/ociobenablement/
-kubectl delete deployment graph-svr-deployment
+docker login -u ${3}/identitycloudservice/${1} -p $2  ${4}.ocir.io
+docker buildx build --platform linux/amd64 --push -t ${4}.ocir.io/ociobenablement/${name}:latest .
+#docker logout ${4}.ocir.io/${3}/
+kubectl delete deployment ${name}-deployment
 sleep 15
 kubectl apply -f ./deployment.yaml
-kubectl apply -f ./graph-svr-svc.yaml
+kubectl apply -f ./${name}.yaml
