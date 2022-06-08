8a55a35a95a5759d01a56e61097080741f599880
Phil Wilkins <phil@mp3monster.org>
2022-06-09 00:54:55 +0100

    changed way parameters are setup and added boolean guard on the console logging

--- graph-svr/ProviderInternalAPI.js
+++ graph-svr/ProviderInternalAPI.js
@@ -8,27 +8,48 @@ export default class ProviderInternalAPI extends RESTDataSource {
   constructor() {
     super();
     let config = JSON.parse(fs.readFileSync('./config.json'));
-    this.baseURL = 'http://' + config['ref-data-svc-base'];
+    this.baseURL = 'http://' + config['ref-data-svc-base']+'/';
     console.log("Ref REST Data Source:"+this.baseURL);
   }
 
   // GET
   async getProvider(code) {
     console.log("getProvider (%s) directing to %s",code,this.baseURL);
-    request.params.set('id', code);
-    return this.get(`provider`);
+    return this.get(`provider?code=${code}`);
+  }
+
+
+    // build up the params part of the url prefixing as appropriate
+  addParam(params, name, value)
+  {
+    if (params == '')
+    {
+      params = params + '?';
+    }
+    else
+    {
+      params = `${params}&`;
+    }
+  
+    params = `${params}${name}=${value}`;
+    return params;
   }
 
   // GET
-  async getProviders() {
-    console.log("getProviders directing to %s",this.baseURL);
-    return this.get(`providers`);
+  async getProviders(code, alias, name) {
+    console.log("getProviders directing to %s", this.baseURL);
+    
+    let params = '';
+    if (code != null) { params = addParam(params, 'code', code); }
+    if (alias != null) { params = addParam(params, 'alias', alias); }
+    if (name != null) { params = addParam(params, 'name', name); }
+    
+    return this.get(`providers${params}`);
   }
 
   // DELETE
-  async deleteProvider(id) {
+  async deleteProvider(code) {
         console.log("deleteProvider directing to %s",id, this.baseURL);
-    request.params.set('id', id);
     return this.delete();
   }
 }
